2a26ab1216855222be8d9a76a150027c96b2e33c
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-08-04 09:24:37 -0300

    TWT export styles to file

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,7 +1,4 @@
-import { fonts, colors, breakpoints } from "../../styles/theme";
-import { addOpacityToColor } from "../../styles/utils";
-
-const backgroundColor = addOpacityToColor(colors.primary, 0.3);
+import styles, { globalStyles } from "./styles";
 
 export default function AppLayout({ children }) {
   return (
@@ -10,50 +7,10 @@ export default function AppLayout({ children }) {
         <main>{children}</main>
       </div>
 
-      <style jsx>
-        {`
-          div {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-          }
-          main {
-            background: #fff;
-            box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
-            border-radius: 10px;
-            height: 100%;
-            width: 100%;
-            padding: 10px;
-          }
-
-          @media (min-width: ${breakpoints.mobile}) {
-            main {
-              width: ${breakpoints.mobile};
-              height: 90vh;
-            }
-          } ;
-        `}
+      <style jsx>{styles}</style>
+      <style jsx global>
+        {globalStyles}
       </style>
-
-      <style jsx global>{`
-        html,
-        body {
-          background-image: radial-gradient(
-              ${backgroundColor} 1px,
-              transparent 1px
-            ),
-            radial-gradient(${backgroundColor} 1px, transparent 1px);
-          background-position: 0 0, 25px 25px;
-          background-size: 50px 50px;
-          padding: 0;
-          margin: 0;
-          font-family: ${fonts.base};
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </>
   );
 }
--- components/AppLayout/styles.js
+++ components/AppLayout/styles.js
@@ -1,1 +1,45 @@
 import css from "styled-jsx/css";
+import { fonts, colors, breakpoints } from "../../styles/theme";
+import { addOpacityToColor } from "../../styles/utils";
+
+const backgroundColor = addOpacityToColor(colors.primary, 0.3);
+
+export const globalStyles = css.global`
+  html,
+  body {
+    background-image: radial-gradient(${backgroundColor} 1px, transparent 1px),
+      radial-gradient(${backgroundColor} 1px, transparent 1px);
+    background-position: 0 0, 25px 25px;
+    background-size: 50px 50px;
+    padding: 0;
+    margin: 0;
+    font-family: ${fonts.base};
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default css`
+  div {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  }
+  main {
+    background: #fff;
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+    height: 100%;
+    width: 100%;
+    padding: 10px;
+  }
+
+  @media (min-width: ${breakpoints.mobile}) {
+    main {
+      width: ${breakpoints.mobile};
+      height: 90vh;
+    }
+  }
+`;
