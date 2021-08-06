5e3aff65d6c8748a7f1169b142af02e9f0f2fb77
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-08-06 15:24:51 -0300

    TWT fix header and nev style

---
+++ components/twity/index.js
@@ -0,0 +1,34 @@
+import Avatar from "../../components/avatar/";
+import { colors } from "../../styles/theme";
+
+export default function Twity({ avatar, username, message }) {
+  return (
+    <>
+      <article>
+        <div>
+          <Avatar src={avatar} height={21} alt={username} />
+        </div>
+        <section>
+          <strong>{username}</strong>
+          <p>{message}</p>
+        </section>
+      </article>
+
+      <style jsx>{`
+        article {
+          display: flex;
+          padding: 10px 15px;
+          border-bottom: 1px solid ${colors.terciary};
+        }
+        div {
+          display: flex;
+          padding: 10px 15px;
+        }
+        p {
+          lineheight: 1.3125;
+          margin: 0;
+        }
+      `}</style>
+    </>
+  );
+}
