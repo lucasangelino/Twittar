29cabc15b1993e4cf209e6900e61c1007edc94c5
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-08-18 10:16:22 -0300

    TWT add 404 page

---
+++ pages/400/400.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Error404 from "../../public/404.gif";
+
+export default function Custom404() {
+  return (
+    <>
+      <Image src={Error404} alt="Error 404 - Content Not Found" />
+      <h1>Content Not Found</h1>
+    </>
+  );
+}
