6afbef33c749e0b7ef4bb07fcd18509ab74f255b
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-11-18 09:34:49 -0300

    TWT remove error

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
 
   const handleLoginWithGitHub = () => {
     loginWithGitHub().catch((err) => {
-      console.log(err);
+      console.error(err);
     });
   };
 
